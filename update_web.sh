9345a56a36014aebf338aa0721ad61081c442fa0
gsmlg <me@gsmlg.org>
2018-07-28 14:40:36 +0800

    update

--- update_web.sh
+++ update_web.sh
@@ -2,5 +2,7 @@
 
 ./do_all.sh tar zxf /mnt/dropbox/gsmlg.tar.gz -C /opt/gsmlg
 
-./do_all.sh 'PORT=10088 LC_ALL=en_US.UTF-8 /opt/gsmlg/bin/gsmlg reboot'
+./do_all.sh 'PORT=10088 LC_ALL=en_US.UTF-8 /opt/gsmlg/bin/gsmlg stop'
+
+./do_all.sh 'PORT=10088 LC_ALL=en_US.UTF-8 /opt/gsmlg/bin/gsmlg start'
 
