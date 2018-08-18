739fc75a21d64a881fed80d69dc2c0204f7220a4
gsmlg <me@gsmlg.org>
2018-08-18 23:38:04 +0800

    update do not need to down services

--- async_update_web.sh
+++ async_update_web.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CMD="cd /var/www/docker-services; docker-compose pull && docker-compose down && docker-compose up -d && docker image prune -f"
+CMD="cd /var/www/docker-services; docker-compose pull && docker-compose up -d && docker image prune -f"
 
 ./async_all.sh "$CMD"
 
--- update_web.sh
+++ update_web.sh
@@ -2,7 +2,7 @@
 
 #dcpull && dcdn && dcup -d && docker image prune -f
 
-CMD="cd /var/www/docker-services; docker-compose pull && docker-compose down && docker-compose up -d && docker image prune -f"
+CMD="cd /var/www/docker-services; docker-compose pull && docker-compose up -d && docker image prune -f"
 
 ./do_all.sh $CMD
 
